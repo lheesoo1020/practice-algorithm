9db2191baccfe193a28afac45c2d8f7037511172
lheesoo1020 <71459841+lheesoo1020@users.noreply.github.com>
2024-09-11 01:00:36 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 15788 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        String[] list = str.split(" ");
+
+        int n = Integer.parseInt(list[0]);
+        int m = Integer.parseInt(list[1]);
+
+        int[] nums = new int[m];
+        boolean[] check = new boolean[n+1];
+
+        dfs(1, 1, n, m, nums, check);
+
+    }
+
+    public static void dfs(int depth, int startnum, int n, int m, int[] nums, boolean[] check) {
+        if (depth == m+1) {
+            for (int num : nums) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = startnum; i <= n; i++) {
+            if (check[i]) continue;
+            check[i] = true;
+            nums[depth-1] = i;
+            dfs(depth+1, i + 1, n, m, nums, check);
+            check[i] = false;
+        }
+    }
+}
