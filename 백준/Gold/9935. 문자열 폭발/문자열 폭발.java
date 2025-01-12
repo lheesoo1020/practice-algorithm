283f422db00bf19d79ef399da71c62bb906395ff
lheesoo1020 <71459841+lheesoo1020@users.noreply.github.com>
2025-01-12 22:58:52 +0900

    [Gold IV] Title: 문자열 폭발, Time: 552 ms, Memory: 43028 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -14,16 +14,22 @@ public class Main {
         boolean flag = false;
 
         for (int i = 0; i < str.length(); i++) {
+            //일단 넣고 확인
             stack1.push(str.charAt(i));
+            //방금 넣은게 폭탄 마지막 글자랑 같으면 flag 설정하고 역순으로 검사
             if (stack1.peek() == ex.charAt(ex.length() - 1)) {
                 flag = true;
+                //검사
                 for (int j = 0; j < ex.length(); j++) {
+                    //폭탄이 아니면 flag 해제
                     if (stack1.isEmpty() || stack1.peek() != ex.charAt(ex.length() - j - 1)) {
                         flag = false;
                         break;
                     }
+                    //폭탄은 stack2에 빼놓기
                     stack2.push(stack1.pop());
                 }
+                //폭탄이 아니었다면 stack2에서 stack1으로 전부 되돌리기
                 if (!flag) {
                     int size = stack2.size();
                     for (int k = 0; k < size; k++) {
