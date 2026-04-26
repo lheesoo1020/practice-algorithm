d9ff2ea6de7bfb0b2e19c449cb414c88adfdf769
lheesoo1020 <71459841+lheesoo1020@users.noreply.github.com>
2026-04-26 13:13:54 +0900

    [level 3] Title: 입국심사, Time: 59.75 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,39 @@
+class Solution {
+    public long solution(int n, int[] times) {
+        
+        long left = 1;
+        long right = (long) getMin(times) * n;
+        long answer = 0;
+        
+        while(left <= right) {
+            long mid = (left + right) / 2;
+            
+            long total = 0;
+            for (int time : times) {
+                total += mid/time;
+            }
+            
+            if (total >= n) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+            
+            
+        }
+        
+        return answer;
+    }
+    
+    private int getMin(int[] times) {
+        int min = times[0];
+        for (int time : times) {
+            if (time < min) {
+                min = time;
+            }
+        }
+        
+        return min;
+    }
+}
